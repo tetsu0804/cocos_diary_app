1466ae8d6c1b52674cf23dd03175d7c4ab80af54
tetsu <banginhoodyonezawa@gmail.com>
2020-09-05 17:31:52 +0900

    Signup初期設置完了 次は本格的に作る

--- app/javascript/components/Header.vue
+++ app/javascript/components/Header.vue
@@ -2,6 +2,7 @@
   <div>
     <b-nav>
       <router-link :to="{ name: 'Home' }">COCO's Diary Application</router-link>
+      <router-link :to="{ name: 'Signup' }">ユーザー登録</router-link>
       <b-nav-item>Link</b-nav-item>
       <b-nav-item>Another Link</b-nav-item>
       <b-nav-item disabled>Disabled</b-nav-item>
--- app/javascript/router/router.js
+++ app/javascript/router/router.js
@@ -1,10 +1,13 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Home from "../components/Home.vue"
+import Signup from "../components/Signup.vue"
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: "/", component: Home, name: "Home"}
+    { path: "/", component: Home, name: "Home"},
+    { path: "/signup", component: Signup, name: "Signup"}
   ]
 })
